58aeeb2c4a28198f80844f093e840dc822814a55
Sophie <wenxi1988@hotmail.com>
2021-12-25 23:28:27 -0500

    add button event at home page

--- app/src/main/java/com/example/nav_datatransferandanimation20211225/HomeFragment.java
+++ app/src/main/java/com/example/nav_datatransferandanimation20211225/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.nav_datatransferandanimation20211225;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -60,5 +61,10 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 }
